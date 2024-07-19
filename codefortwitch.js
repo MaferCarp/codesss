8670b16f82f8a900095b3f08be6a9d2fb2a71255
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-19 17:21:25 -0300

    Create codefortwitch.js

---
+++ codefortwitch.js
@@ -0,0 +1,8 @@
+if (pregunta.includes("capitan") || pregunta.includes("capitán") || pregunta.includes("capitana")) {
+    ("Los capitanes son Karina de MataFama y Pedro de Team Black. @" + usuario);
+} else if (pregunta.includes("líder") || pregunta.includes("líderes")) {
+    ("Los líderes de la semana son Pedro y Carlyn. @" + usuario);
+} else {
+    ("MrDestructoid");
+}                           
+    
