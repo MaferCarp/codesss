7100a13f791ef5ab7861b4b1906a20deb349d2b6
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-23 17:15:30 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -1,14 +1,14 @@
 if (pregunta.includes("capitan") || pregunta.includes("capitán") || pregunta.includes("capitana") || pregunta.includes("capitanes")) {
     ("Los capitanes son Karina (MataFama) y Pedro (Team Black)... @" + usuario);
 } else if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres") || pregunta.includes("liderazgo")) {
-    ("Los líderes de la semana son Carlyn ♀ y Pedro ♂ ... go to https://youtu.be/Si5luuFaJd8?si=KhF2SI0a3RS5kuJB (CARLYN) , o https://youtu.be/ta4yYsXjPyo?si=DMEKB5yMcWnu3g-m (PEDRO) @" + usuario); }  
+    ("Los líderes de la semana son Carlyn ♀ y Pedro ♂ ... Clip → https://youtu.be/Si5luuFaJd8?si=KhF2SI0a3RS5kuJB (CARLYN) , o https://youtu.be/ta4yYsXjPyo?si=DMEKB5yMcWnu3g-m (PEDRO) @" + usuario); }  
      else if (pregunta.includes("weon") || pregunta.includes("weona") || pregunta.includes("weones") || pregunta.includes("weón")) {
     ("Diego, Felipe, Seba ... @" + usuario); } 
          else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("gano la competencia") || pregunta.includes("competencia de equipos")) {
-    ("Matafama son los ganadores... go to https://youtu.be/UiQkR-oOmHU?si=zKf_N2rupoteYSV6 @" + usuario); }
+    ("Matafama son los ganadores... Clip → https://youtu.be/UiQkR-oOmHU?si=zKf_N2rupoteYSV6 @" + usuario); }
     else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("eliminada") || pregunta.includes("eliminados")) {
-    ("La eliminada fue Daniela(66%) vs Felipe(34%)... go to https://youtu.be/yNs1yWh9hU4?si=nSwBn3EnAN8XzR64 @" + usuario); }
+    ("La eliminada fue Daniela(66%) vs Felipe(34%)... Clip → https://youtu.be/yNs1yWh9hU4?si=nSwBn3EnAN8XzR64 @" + usuario); }
      else if (pregunta.includes("nominado") || pregunta.includes("placa") || pregunta.includes("nominados")) {
-    ("Los nominados son Valentina (13), Daniela(9), Michelle(7), Manuel(4), Felipe(4) ...go to https://youtu.be/yyhnLu5OdkE?si=KgxsttxByknAt-tq Camila Andrade salvada ... @" + usuario);
+    ("Los nominados son Valentina (13), Daniela(9), Michelle(7), Manuel(4), Felipe(4) ... Clip → https://youtu.be/yyhnLu5OdkE?si=KgxsttxByknAt-tq Camila Andrade salvada ... @" + usuario);
 } else {
 }                           
