6de3eb72eca7539d2bfb66f5da695ccc9ee48da2
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-23 16:22:57 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -1,7 +1,7 @@
 if (pregunta.includes("capitan") || pregunta.includes("capitán") || pregunta.includes("capitana") || pregunta.includes("capitanes")) {
     ("Los capitanes son Karina de MataFama y Pedro de Team Black... @" + usuario);
 } else if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres")) {
-    ("Los líderes de la semana son Pedro y Carlyn... @" + usuario); }  
+    ("Los líderes de la semana son Carlyn ♀ y Pedro ♂ ... go to https://youtu.be/Si5luuFaJd8?si=KhF2SI0a3RS5kuJB (CARLYN) , o https://youtu.be/ta4yYsXjPyo?si=DMEKB5yMcWnu3g-m (PEDRO) @" + usuario); }  
      else if (pregunta.includes("weon") || pregunta.includes("weona") || pregunta.includes("weones") || pregunta.includes("weón")) {
     ("Diego, Felipe, Seba ... @" + usuario); } 
     else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("eliminada") || pregunta.includes("eliminados")) {
