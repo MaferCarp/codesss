8979bac23b458b2615a4761b36db62234a5dfdc4
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-23 16:31:38 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -1,9 +1,11 @@
 if (pregunta.includes("capitan") || pregunta.includes("capitán") || pregunta.includes("capitana") || pregunta.includes("capitanes")) {
-    ("Los capitanes son Karina de MataFama y Pedro de Team Black... @" + usuario);
-} else if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres")) {
+    ("Los capitanes son Karina (MataFama) y Pedro (Team Black)... @" + usuario);
+} else if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres") || pregunta.includes("liderazgo")) {
     ("Los líderes de la semana son Carlyn ♀ y Pedro ♂ ... go to https://youtu.be/Si5luuFaJd8?si=KhF2SI0a3RS5kuJB (CARLYN) , o https://youtu.be/ta4yYsXjPyo?si=DMEKB5yMcWnu3g-m (PEDRO) @" + usuario); }  
      else if (pregunta.includes("weon") || pregunta.includes("weona") || pregunta.includes("weones") || pregunta.includes("weón")) {
     ("Diego, Felipe, Seba ... @" + usuario); } 
+         else if (pregunta.includes("competencia") || pregunta.includes("equipos") || pregunta.includes("prueba de equipos") || pregunta.includes("competencia de equipos")) {
+    ("Matafama son los ganadores... go to https://youtu.be/UiQkR-oOmHU?si=zKf_N2rupoteYSV6 @" + usuario); }
     else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("eliminada") || pregunta.includes("eliminados")) {
     ("La eliminada fue Daniela(66%) vs Felipe(34%)... go to https://youtu.be/yNs1yWh9hU4?si=nSwBn3EnAN8XzR64 @" + usuario); }
      else if (pregunta.includes("nominado") || pregunta.includes("placa") || pregunta.includes("nominados")) {
