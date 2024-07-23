30c8a99c21f422dce141b4581d513bbdfe74db99
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-23 17:11:18 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -4,7 +4,7 @@ if (pregunta.includes("capitan") || pregunta.includes("capitán") || pregunta.in
     ("Los líderes de la semana son Carlyn ♀ y Pedro ♂ ... go to https://youtu.be/Si5luuFaJd8?si=KhF2SI0a3RS5kuJB (CARLYN) , o https://youtu.be/ta4yYsXjPyo?si=DMEKB5yMcWnu3g-m (PEDRO) @" + usuario); }  
      else if (pregunta.includes("weon") || pregunta.includes("weona") || pregunta.includes("weones") || pregunta.includes("weón")) {
     ("Diego, Felipe, Seba ... @" + usuario); } 
-         else if (pregunta.includes("competencia") || pregunta.includes("equipos") || pregunta.includes("prueba de equipos") || pregunta.includes("competencia de equipos")) {
+         else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("gano la competencia") || pregunta.includes("competencia de equipos")) {
     ("Matafama son los ganadores... go to https://youtu.be/UiQkR-oOmHU?si=zKf_N2rupoteYSV6 @" + usuario); }
     else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("eliminada") || pregunta.includes("eliminados")) {
     ("La eliminada fue Daniela(66%) vs Felipe(34%)... go to https://youtu.be/yNs1yWh9hU4?si=nSwBn3EnAN8XzR64 @" + usuario); }
