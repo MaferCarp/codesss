e2291db8a629e113120b44774c593d3760081a0f
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-26 02:11:43 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -5,7 +5,7 @@ if (pregunta.includes("capitan") || pregunta.includes("capitán") || pregunta.in
      else if (pregunta.includes("weon") || pregunta.includes("weona") || pregunta.includes("weones") || pregunta.includes("weón")) {
     ("Diego, Felipe, Seba ... @" + usuario); } 
          else if (pregunta.includes("salvo") || pregunta.includes("salvó") || pregunta.includes("salvaron") || pregunta.includes("salvada") || pregunta.includes("salvado") || pregunta.includes("salvados")) {
-    ("Chama salvada por Prueba de salvación ... @" + usuario); }
+    ("Chama salvada por Prueba de salvación y Yuyuniz por voto de los líderes ... @" + usuario); }
          else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("gano la competencia") || pregunta.includes("competencia de equipos")) {
     ("Matafama son los ganadores... Clip → https://youtu.be/Odu7NPYEdMg?si=OlUxQFUMM5PxObo5 @" + usuario); }
     else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("eliminada") || pregunta.includes("eliminados")) {
