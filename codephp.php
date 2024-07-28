e1d81cfeb91d5ec746118e0626db056059f53ab2
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-28 17:05:23 -0300

    Create codephp.php

---
+++ codephp.php
@@ -0,0 +1,13 @@
+
+<?php
+    $numero = rand(0, 100);
+    $mensaje = "";
+
+    if($numero == 0){
+        $mensaje = "Te quiere como amigo y de lejos";
+    }else{
+        $mensaje = $numero;
+    }
+    echo $mensaje;
+?>
+                            
