f72369db7bc9f1692500dd9d7edb1b42ce128d52
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-28 23:34:09 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -3,9 +3,6 @@ if (pregunta.includes("capitan") || pregunta.includes("capitán") || pregunta.in
     else if (pregunta.includes("weon") || pregunta.includes("weona") || pregunta.includes("weones") || pregunta.includes("weón")) {("Diego, Felipe, Seba ... @" + usuario); } 
     else if (pregunta.includes("salvo") || pregunta.includes("salvó") || pregunta.includes("salvaron") || pregunta.includes("salvada") || pregunta.includes("salvado") || pregunta.includes("salvados")) { ("Chama salvada por Prueba de salvación y Yuyuniz por voto de los líderes ... @" + usuario); }
          else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("gano la competencia") || pregunta.includes("competencia de equipos")) {("Matafama son los ganadores... Clip → https://youtu.be/Odu7NPYEdMg?si=OlUxQFUMM5PxObo5 @" + usuario); }
-    else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("eliminada") || pregunta.includes("eliminados")) {
-    ("La eliminada fue Daniela(66%) vs Felipe(34%)... Clip → https://youtu.be/yNs1yWh9hU4?si=nSwBn3EnAN8XzR64 @" + usuario); }
-     else if (pregunta.includes("nominado") || pregunta.includes("votaciones") || pregunta.includes("nominaron") || pregunta.includes("votación") || pregunta.includes("votaron") || pregunta.includes("placa") || pregunta.includes("nominados")) {
-    (" Patricio, Michelle, Karina, y Camila Andrade  ... @" + usuario);
-} else {
-;}                           
+    else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("eliminada") || pregunta.includes("eliminados")) {("La eliminada fue Daniela(66%) vs Felipe(34%)... Clip → https://youtu.be/yNs1yWh9hU4?si=nSwBn3EnAN8XzR64 @" + usuario); }
+     else if (pregunta.includes("nominado") || pregunta.includes("votaciones") || pregunta.includes("nominaron") || pregunta.includes("votación") || pregunta.includes("votaron") || pregunta.includes("placa") || pregunta.includes("nominados")) {(" Patricio, Michelle, Karina, y Camila Andrade  ... @" + usuario);} 
+     else {}                           
