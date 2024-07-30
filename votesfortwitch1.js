a46e8a09d8f012dbc40c9dcfbcb87a051fbdd439
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-29 23:13:19 -0300

    Update votesfortwitch1.js

--- votesfortwitch1.js
+++ votesfortwitch1.js
@@ -1,2 +1,2 @@
-if (pregunta.includes("michelle")||pregunta.includes("michel")) {( usuario + `  ` + " Michelle"+ `  ` + cuenta + `  ` + "votos");}
+if (pregunta.includes("michelle") && pregunta.includes("michel")) {( usuario + `  ` + " Michelle"+ `  ` + cuenta + `  ` + "votos");}
 else {("Los nominados son Michelle, Patricio, Karina y Camila Andrade"+`  `+usuario);}
