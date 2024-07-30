6ddacdddc8817a570fcb911b0a224d0cf83a657f
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-29 22:51:37 -0300

    Create votefortwitch4.js

---
+++ votefortwitch4.js
@@ -0,0 +1,5 @@
+if (pregunta.includes("michelle")||pregunta.includes("michel")) {( usuario + `  ` + " Michelle"+ `  ` + cuenta + `  ` + "votos");}
+else if (pregunta.includes("patricio") || pregunta.includes("pato")) {( usuario + `  ` + "Patricio"+ `  ` + cuenta + `  ` + "votos" );}
+else if(pregunta.includes("karina")  || pregunta.includes("kari")) {( usuario + `  ` + " Karina"+`  `+ cuenta + `  ` + "votos" );}
+else if(pregunta.includes("Camila") || pregunta.includes("Andrade"))  {( usuario + `  ` + "Camila Andrade"+ `  ` + cuenta + `  ` + "votos" );}
+else {("Los nominados son Michelle, Patricio, Karina y Camila Andrade"+`  `+usuario);}
