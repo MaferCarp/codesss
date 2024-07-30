58a7160c59625f8dee611c3333e97919393f00e6
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-29 22:09:24 -0300

    Create códigofortwitch3.js

---
+++ códigofortwitch3.js
@@ -0,0 +1,6 @@
+if (pregunta.includes("michelle")||pregunta.includes("michel")) {(" Michelle" + cuenta + usuario);}
+else if (pregunta.includes("competencia") || pregunta.includes("equipos")) {( usuario + "Competencia semanal → Martes... " );}
+else if(pregunta.includes("nominación")  || pregunta.includes("votación") || pregunta.includes("nominan") || pregunta.includes("votan") || pregunta.includes("nomin")) {( usuario + " Gala de nominación → Miércoles... " );}
+else if(pregunta.includes("salvación") || pregunta.includes("salvan"))  {( usuario + " Prueba de salvación → Jueves... " );}
+else if(pregunta.includes("fiesta") || pregunta.includes("baile")) {( usuario + " Espiando la casa: La fiesta → Viernes... " );}
+else {}
