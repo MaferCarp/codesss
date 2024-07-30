614a34a1049ec578778c86749bf94c7b81067df7
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-29 22:17:18 -0300

    Update códigofortwitch3.js

--- códigofortwitch3.js
+++ códigofortwitch3.js
@@ -1,5 +1,5 @@
-if (pregunta.includes("michelle")||pregunta.includes("michel")) {(" Michelle" + cuenta + usuario);}
-else if (pregunta.includes("competencia") || pregunta.includes("equipos")) {( usuario + "Competencia semanal → Martes... " );}
+if (pregunta.includes("michelle")||pregunta.includes("michel")) {( usuario + " Michelle" + cuenta + "votos");}
+else if (pregunta.includes("patricio") || pregunta.includes("pato")) {( usuario + "Patricio" + cuenta + "votos" );}
 else if(pregunta.includes("nominación")  || pregunta.includes("votación") || pregunta.includes("nominan") || pregunta.includes("votan") || pregunta.includes("nomin")) {( usuario + " Gala de nominación → Miércoles... " );}
 else if(pregunta.includes("salvación") || pregunta.includes("salvan"))  {( usuario + " Prueba de salvación → Jueves... " );}
 else if(pregunta.includes("fiesta") || pregunta.includes("baile")) {( usuario + " Espiando la casa: La fiesta → Viernes... " );}
