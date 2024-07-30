a63af1cb068c026bfacba12639f68028890a0e79
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-07-29 22:22:16 -0300

    Update códigofortwitch3.js

--- códigofortwitch3.js
+++ códigofortwitch3.js
@@ -1,4 +1,4 @@
-if (pregunta.includes("michelle")||pregunta.includes("michel")) {( usuario + " Michelle  →" + cuenta + "  votos");}
+if (pregunta.includes("michelle")||pregunta.includes("michel")) {( usuario + " " + " Michelle"+" " + cuenta +" "+ "  votos");}
 else if (pregunta.includes("patricio") || pregunta.includes("pato")) {( usuario + "Patricio" + cuenta + "votos" );}
 else if(pregunta.includes("nominación")  || pregunta.includes("votación") || pregunta.includes("nominan") || pregunta.includes("votan") || pregunta.includes("nomin")) {( usuario + " Gala de nominación → Miércoles... " );}
 else if(pregunta.includes("salvación") || pregunta.includes("salvan"))  {( usuario + " Prueba de salvación → Jueves... " );}
