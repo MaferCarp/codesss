fb16565da6a566498df2fc3fa9fd88d066a038c5
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-01 23:43:09 -0300

    Update hour.js

--- hour.js
+++ hour.js
@@ -1,2 +1,2 @@
-if (pregunta.includes("empieza") || pregunta.includes("comienza") || pregunta.includes("arranca")) {(" La gala en vivo comienza en "+" "+ timer +" "+ usuario+" "+"BangbooBounce" );}
+if (pregunta.some("empieza") || pregunta.includes("comienza") || pregunta.includes("arranca")) {(" La gala en vivo comienza en "+" "+ timer +" "+ usuario+" "+"BangbooBounce" );}
 else {}
