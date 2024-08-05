70b89427b17e18211af8ed4623fe337c055b41f8
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-05 02:39:42 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -8,7 +8,7 @@ if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.inc
         
          else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("ganó la competencia") || pregunta.includes("competencia de equipos")) {(" !Linda !Chama !Carlyn !Manuel !Pedro !Íñigo !Waldo !Felipe !Yuyuniz " + usuario); }
              
-    else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("elimin") || pregunta.includes("eliminados")) {("La eliminada fue Karina (86,30%) vs Cami andrade (13,70%)...clip → https://youtu.be/nulOjomyCRk?si=N59z2ExxIE8cEzir " + usuario); }
+    else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("elimin") || pregunta.includes("eliminados")) {("La eliminada fue Yuyuniz (86,30%) sobre Patricio (13,70%)... " + usuario); }
         
      else if (pregunta.includes("nominado") || pregunta.includes("votaciones") || pregunta.includes("nominaron") || pregunta.includes("votación") || pregunta.includes("votaron") || pregunta.includes("placa") || pregunta.includes("nominados")) {(" Yuyuniz, Manuel, Chama, Cami y Patricio (Michelle & Felipe salvad@s) ... " + usuario);} 
          
