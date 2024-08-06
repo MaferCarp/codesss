141a48a7d3a62b932fc7669e43dfdeede8331130
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-06 14:36:43 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -6,7 +6,7 @@ if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.inc
          
     else if (pregunta.includes("salv") || pregunta.includes("salvó") || pregunta.includes("salvaron") || pregunta.includes("salvada") || pregunta.includes("salvado") || pregunta.includes("salvados")) { (" Felipe salvad@ en la Prueba de salvación y Michelle por Líder ... " + usuario); }
         
-         else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("ganó la competencia") || pregunta.includes("competencia de equipos")) {(" !Linda !Chama !Carlyn !Manuel !Pedro !Íñigo !Waldo !Felipe !Yuyuniz " + usuario); }
+         else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("ganó la competencia") || pregunta.includes("competencia de equipos") || pregunta.includes("casa")) {(" !Waldo, Miguel, Pato,Íñigo, Iván, Linda, Cami A., Michelle y Carlyn (Casa) " + usuario); }
              
     else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("elimin") || pregunta.includes("eliminados")) {("La eliminada fue Yuyuniz (86,30%) sobre Patricio (13,70%)... " + usuario); }
         
