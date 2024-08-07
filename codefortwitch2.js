fe5f6b973d7564308461305d6d178fdfb67ce5c1
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-07 20:39:06 -0300

    Update codefortwitch2.js

--- codefortwitch2.js
+++ codefortwitch2.js
@@ -1,7 +1,12 @@
-if (pregunta.includes("capítulo")||pregunta.includes("episodio") ||pregunta.includes("prime") ||pregunta.includes("transmisión") || pregunta.includes("reality")) {(" Gala de nominación...23:00 Hs")//+ timer2// ;}
-  else if (pregunta.includes("eliminación") || pregunta.includes("elimin") || pregunta.includes("gala")) {("Gala de eliminación (Domingo) ")//+ timer1// ;}
+if (pregunta.includes("capítulo")||pregunta.includes("episodio") ||pregunta.includes("prime") ||pregunta.includes("transmisión") || pregunta.includes("reality")) {(" Gala de nominación...23:00 Hs") ;} /+ timer2/
+                                                                                                                                                                   
+  else if (pregunta.includes("eliminación") || pregunta.includes("elimin") || pregunta.includes("gala")) {("Gala de eliminación (Domingo) ") ;} /+timer1/
+
 else if (pregunta.includes("competencia") || pregunta.includes("equipos")) {( "Competencia semanal → Martes... " );}
+  
 else if(pregunta.includes("nominación")  || pregunta.includes("votación") || pregunta.includes("nominan") || pregunta.includes("votan") || pregunta.includes("nomin")) {( " Gala de nominación → Miércoles. " );}
+  
 else if(pregunta.includes("salvación") || pregunta.includes("salvan"))  {(" Prueba de salvación → Jueves. " );}
+  
 else if(pregunta.includes("fiesta") || pregunta.includes("baile")) {(" Espiando la casa: La fiesta → Viernes. " );}
 else {}
