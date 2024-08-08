b8df48020dce3aba1511e8b1cb896247cb569234
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-08 00:07:52 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -1,13 +1,13 @@
 if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres") || pregunta.includes("liderazgo")) {("El nuevo líder es !iñigo."); }  
     
- else if (pregunta.includes("espontanea") || pregunta.includes("espontaneo") || pregunta.includes("espontánea") || pregunta.includes("espont")) {("!Chama activó el voto espontáneo. Dio 3!Felipe y 2!Yuyuniz ... " + usuario); }   
+ else if (pregunta.includes("espontanea") || pregunta.includes("espontaneo") || pregunta.includes("espontánea") || pregunta.includes("espont")) {("Manuel activó el voto espontáneo con 3 iñigo y 2 Felipe. "); }   
          
-    else if (pregunta.includes("salv") || pregunta.includes("salvó") || pregunta.includes("salvaron") || pregunta.includes("salvada") || pregunta.includes("salvado") || pregunta.includes("salvados")) { (" Felipe salvad@ en la Prueba de salvación y Michelle por Líder ... " + usuario); }
+    else if (pregunta.includes("salv") || pregunta.includes("salvó") || pregunta.includes("salvaron") || pregunta.includes("salvada") || pregunta.includes("salvado") || pregunta.includes("salvados")) { (" Felipe salvad@ en la Prueba de salvación y Michelle por Líder. "); }
         
-         else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("ganó la competencia") || pregunta.includes("competencia de equipos") || pregunta.includes("casa")) {(" !Waldo, Miguel, Pato,Íñigo, Iván, Linda, Cami A., Michelle y Carlyn (Casa) " + usuario); }
+         else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("ganó la competencia") || pregunta.includes("competencia de equipos") || pregunta.includes("casa")) {(" !Waldo, Miguel, Pato,Íñigo, Iván, Linda, Cami A., Michelle y Carlyn (Casa) "); }
              
-    else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("elimin") || pregunta.includes("eliminados")) {("La eliminada fue Yuyuniz (86,30%) sobre Patricio (13,70%)... " + usuario); }
+    else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("elimin") || pregunta.includes("eliminados")) {("La eliminada fue Yuyuniz (86,30%) sobre Patricio (13,70%). "); }
         
-     else if (pregunta.includes("nominado") || pregunta.includes("votaciones") || pregunta.includes("nominaron") || pregunta.includes("votación") || pregunta.includes("votaron") || pregunta.includes("placa") || pregunta.includes("nominados")) {(" Yuyuniz, Manuel, Chama, Cami y Patricio (Michelle & Felipe salvad@s) ... " + usuario);} 
+     else if (pregunta.includes("nominado") || pregunta.includes("votaciones") || pregunta.includes("nominaron") || pregunta.includes("votación") || pregunta.includes("votaron") || pregunta.includes("placa") || pregunta.includes("nominados")) {(" Yuyuniz, Manuel, Chama, Cami y Patricio (Michelle & Felipe salvad@s). ");} 
          
      else {}                           
