a2f195a5db452b4585670846ce47cb4594092c2c
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-09 02:22:23 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -2,12 +2,12 @@ if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.inc
     
  else if (pregunta.includes("espontanea") || pregunta.includes("espontaneo") || pregunta.includes("espontánea") || pregunta.includes("espont")) {("Manuel activó el voto espontáneo con 3 iñigo y 2 Felipe. "); }   
          
-    else if (pregunta.includes("salv") || pregunta.includes("salvó") || pregunta.includes("salvaron") || pregunta.includes("salvada") || pregunta.includes("salvado") || pregunta.includes("salvados")) { (" Carlyn salvada en la Prueba de salvación . "); }
+    else if (pregunta.includes("salv") || pregunta.includes("salvó") || pregunta.includes("salvaron") || pregunta.includes("salvada") || pregunta.includes("salvado") || pregunta.includes("salvados")) { (" Carlyn salvada en la Prueba de salvación, y Power salvada por íñigo(líder) . "); }
         
          else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("ganó la competencia") || pregunta.includes("competencia de equipos") || pregunta.includes("casa")) {(" !Waldo, Miguel, Pato,Íñigo, Iván, Linda, Cami A., Michelle y Carlyn (Casa) "); }
              
     else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("elimin") || pregunta.includes("eliminados")) {("La eliminada fue Yuyuniz (86,30%) sobre Patricio (13,70%). "); }
         
-     else if (pregunta.includes("nominado") || pregunta.includes("votaciones") || pregunta.includes("nominaron") || pregunta.includes("votación") || pregunta.includes("votaron") || pregunta.includes("placa") || pregunta.includes("nominados")) {(" Carlyn..Power..Manuel..Chama..Michelle. ");} 
+     else if (pregunta.includes("nominado") || pregunta.includes("votaciones") || pregunta.includes("nominaron") || pregunta.includes("votación") || pregunta.includes("votaron") || pregunta.includes("placa") || pregunta.includes("nominados")) {(" Manuel..Chama..Michelle, y Diego(telefono rojo de Yuhui) ");} 
          
      else {}                           
