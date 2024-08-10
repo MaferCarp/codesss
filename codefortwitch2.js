280231bb4625bb7851fffcf69b947636033c327d
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-09 22:04:50 -0300

    Update codefortwitch2.js

--- codefortwitch2.js
+++ codefortwitch2.js
@@ -1,4 +1,4 @@
-if (pregunta.includes("capítulo")||pregunta.includes("episodio") ||pregunta.includes("prime") ||pregunta.includes("transmisión") || pregunta.includes("reality") ||pregunta.includes("directo")) {(" Espiando la casa: La fiesta en " + timer3) ;} 
+if (pregunta.includes("capítulo")||pregunta.includes("episodio") ||pregunta.includes("prime") ||pregunta.includes("transmisión") || pregunta.includes("reality") ||pregunta.includes("directo")) {(" Espiando la casa: La fiesta en " + timer2) ;} 
                                                                                                                                                                    
   else if (pregunta.includes("eliminación") || pregunta.includes("elimin") || pregunta.includes("gala")) {("Gala de eliminación en "+ timer1) ;} 
 
