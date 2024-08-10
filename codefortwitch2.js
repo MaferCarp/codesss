de8f4d90d2f6ecda8f6f00ad0002261d54fb1009
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-09 21:44:25 -0300

    Update codefortwitch2.js

--- codefortwitch2.js
+++ codefortwitch2.js
@@ -1,6 +1,6 @@
-if (pregunta.includes("capítulo")||pregunta.includes("episodio") ||pregunta.includes("prime") ||pregunta.includes("transmisión") || pregunta.includes("reality") ||pregunta.includes("directo")) {(" Espiando la casa: La fiesta → Viernes.") ;} 
+if (pregunta.includes("capítulo")||pregunta.includes("episodio") ||pregunta.includes("prime") ||pregunta.includes("transmisión") || pregunta.includes("reality") ||pregunta.includes("directo")) {(" Espiando la casa: La fiesta en " + timer3) ;} 
                                                                                                                                                                    
-  else if (pregunta.includes("eliminación") || pregunta.includes("elimin") || pregunta.includes("gala")) {("Gala de eliminación (Domingo) ") ;} 
+  else if (pregunta.includes("eliminación") || pregunta.includes("elimin") || pregunta.includes("gala")) {("Gala de eliminación en "+ timer1) ;} 
 
 else if (pregunta.includes("competencia") || pregunta.includes("equipos")) {( "Competencia semanal → Martes... " );}
   
