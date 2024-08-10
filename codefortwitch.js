2abb996037bd2b093f0787b655c0c6fd6b5a244d
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-09 22:41:20 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -8,6 +8,6 @@ if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.inc
              
     else if (pregunta.includes("eliminado") || pregunta.includes("eliminaron") || pregunta.includes("elimin") || pregunta.includes("eliminados")) {("La eliminada fue Yuyuniz (86,30%) sobre Patricio (13,70%). "); }
         
-     else if (pregunta.includes("nominado") || pregunta.includes("votaciones") || pregunta.includes("nominaron") || pregunta.includes("votación") || pregunta.includes("votaron") || pregunta.includes("placa") || pregunta.includes("nominados")) {(" Manuel..Chama..Michelle, y Diego(telefono rojo de Yuhui) ");} 
+     else if (pregunta.includes("nominado") || pregunta.includes("votaciones") || pregunta.includes("nominaron") || pregunta.includes("votación") || pregunta.includes("votaron") || pregunta.includes("placa") || pregunta.includes("nominados")) {(" Manuel..Chama..Michelle, y Diego (SMS al 3331, o en la web → https://granhermano.celcom.cl/# )");} 
          
      else {}                           
