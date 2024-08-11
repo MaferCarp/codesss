6efae26a24594589492247910fdbec3ed023b41d
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-11 15:03:24 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -2,13 +2,13 @@ if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.inc
     
  else if (pregunta.includes("espontanea") || pregunta.includes("espontaneo") || pregunta.includes("espontánea") || pregunta.includes("espont")) {("Manuel activó el voto espontáneo con 3 iñigo y 2 Felipe. "); }   
          
-    else if (pregunta.includes("salv") || pregunta.includes("salvó") || pregunta.includes("qui") && pregunta.includes("salvaron") || pregunta.includes("cu") && pregunta.includes("salvaron") || pregunta.includes("salvada") || pregunta.includes("salvado") || pregunta.includes("salvados")) { (" Carlyn salvada en la Prueba de salvación, y Power salvada por íñigo(líder) . "); }
+    else if (pregunta.includes("salvó") || pregunta.includes("qui") && pregunta.includes("salvaron") || pregunta.includes("cu") && pregunta.includes("salvaron") || pregunta.includes("salvad")) { (" Carlyn salvada en la Prueba de salvación, y Power salvada por íñigo(líder) . "); }
         
          else if (pregunta.includes("ganó la competencia") || pregunta.includes("equipos") || pregunta.includes("ganaron la competencia") || pregunta.includes("prueba de equipos") || pregunta.includes("ganó la competencia") || pregunta.includes("competencia de equipos") || pregunta.includes("casa")) {(" !Waldo, Miguel, Pato,Íñigo, Iván, Linda, Cami A., Michelle y Carlyn (Casa) "); }
              
-    else if (pregunta.includes("qui") && pregunta.includes("eliminaron") || pregunta.includes("cu") && pregunta.includes("eliminaron")  || pregunta.includes("eliminado") || pregunta.includes("elimin") || pregunta.includes("eliminados")) {("La eliminada fue Yuyuniz (86,30%) sobre Patricio (13,70%). "); }
+    else if (pregunta.includes("qui") && pregunta.includes("eliminaron") || pregunta.includes("cu") && pregunta.includes("eliminaron")  || pregunta.includes("eliminad")) {("La eliminada fue Yuyuniz (86,30%) sobre Patricio (13,70%). "); }
         
-     else if (pregunta.includes("qui") && pregunta.includes("nominaron") || pregunta.includes("cu") && pregunta.includes("nominaron")  || pregunta.includes("qui") && pregunta.includes("votaron") || pregunta.includes("cu") && pregunta.includes("votaron")  ||  pregunta.includes("nominado")   || pregunta.includes("votaciones") || pregunta.includes("votación")  || pregunta.includes("placa") || pregunta.includes("nominados")) {(" Manuel..Chama..Michelle, y Diego (SMS al 3331, o en la web → https://granhermano.celcom.cl/# )");} 
+     else if (pregunta.includes("qui") && pregunta.includes("nominaron") || pregunta.includes("cu") && pregunta.includes("nominaron")  || pregunta.includes("qui") && pregunta.includes("votaron") || pregunta.includes("cu") && pregunta.includes("votaron")  ||  pregunta.includes("nominad")   || pregunta.includes("votac") || pregunta.includes("placa")) {(" Manuel..Chama..Michelle, y Diego (SMS al 3331, o en la web → https://granhermano.celcom.cl/# )");} 
 
           else if (pregunta.includes("temática") || pregunta.includes("tematica")) {("La temática de la fiesta es: Blanco & Negro. "); }
         
