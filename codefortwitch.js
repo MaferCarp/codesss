5c80f043a2f6f17644437261b1fe4adb01f0d40d
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-11 16:58:20 -0300

    Update codefortwitch.js

--- codefortwitch.js
+++ codefortwitch.js
@@ -1,4 +1,4 @@
-if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres") || pregunta.includes("liderazgo")) {("El nuevo líder es !iñigo."); }  
+if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres") || pregunta.includes("liderazgo")) {("El nuevo líder es !iñigo. "+random); }  
     
  else if (pregunta.includes("espontanea") || pregunta.includes("espontaneo") || pregunta.includes("espontánea") || pregunta.includes("espont")) {("Manuel activó el voto espontáneo con 3 iñigo y 2 Felipe. "); }   
          
