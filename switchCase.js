e931278ac38fc544300d8f9a204f137d296ba36f
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-12 22:14:31 -0300

    Update switchCase.js

--- switchCase.js
+++ switchCase.js
@@ -1,5 +1,3 @@
-switch (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres")  pregunta.includes("liderazgo"); (pregunta.includes("espontanea") || pregunta.includes("espontaneo") || pregunta.includes("espontánea") || pregunta.includes("espont")
-       )
-    case 0: {("El nuevo líder es !iñigo. "); }   // quien/es, cual/es 
-  break;
-    case 1: {("Manuel activó el voto espontáneo con 3 iñigo y 2 Felipe. "); }   
+for (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres")  pregunta.includes("liderazgo"); (pregunta.includes("espontanea") || pregunta.includes("espontaneo") || pregunta.includes("espontánea") || pregunta.includes("espont")
+       ) {"El nuevo líder es !iñigo. "; "Manuel activó el voto espontáneo con 3 iñigo y 2 Felipe. "; }   
+    // quien/es, cual/es 
