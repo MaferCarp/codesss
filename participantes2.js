9daec5fe12a6170089b1e3ca616da7cf27b8fde4
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-27 05:11:45 -0300

    Update participantes2.js///

--- participantes2.js
+++ participantes2.js
@@ -1,4 +1,8 @@
 if (pregunta.includes("manu") || pregunta.includes("italiano")) {(" achepto " );}
   else if (pregunta.includes("pedro")) {(" Pedro Pedro Pedro PEDRO PE! SingsNote " );}
- else if (pregunta.includes("waldo")) {(" carrrcul Kappa " );}    
+ else if (pregunta.includes("waldo")) {(" carrrcul Kappa " );}  
+   else if (pregunta.includes("felipe")) {(" 	Sapo parao :V PopNemo " );}
+     else if (pregunta.includes("ivan") || pregunta.includes("iván")) {(" ivan lo hizo antes Kappa " );}
+     	else if (pregunta.includes("iñigo")) {(" encerré a mi nana PopNemo " );}
+        	else if (pregunta.includes("yuhui")) {(" Te tengo miedo? Imposible! " );}
 else {}
