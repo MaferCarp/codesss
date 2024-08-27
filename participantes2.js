b69d3856881c033860dd1467868d346460213978
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-27 03:51:09 -0300

    Update participantes2.js

--- participantes2.js
+++ participantes2.js
@@ -1,10 +1,4 @@
 if (pregunta.includes("manu") || pregunta.includes("italiano")) {(" achepto " );}
   if else (pregunta.includes("pedro")) {(" Pedro Pedro Pedro PEDRO PE! SingsNote " );}
-
-
-
-
-
-
-    
+if else (pregunta.includes("waldo")) {(" carrrcul Kappa " );}    
 else {}
