94d7486f1b3b5aba98ced77099f567e18a0d9ce8
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-27 03:44:11 -0300

    Update participantes.js

--- participantes.js
+++ participantes.js
@@ -1,7 +1,7 @@
 
 if (pregunta.includes("angelica") || pregunta.includes("angélica")) {(" 	💩😭 " );}
   if else (pregunta.includes("carlyn")) {("te conozco hace 3 vidas" );}
- if else (pregunta.includes("linda") || (pregunta.includes("gitana")) {("	la perlan't" );}
+ if else (pregunta.includes("linda") || pregunta.includes("gitana")) {("	la perlan't" );}
 if else (pregunta.includes("mich")) {("		producción ped*rra " );}
 if else (pregunta.includes("power") ||pregunta.includes("ppt")) {("		que injusto nooo que injustoo " );}
 if else (pregunta.includes("waldo")) {("	carrrcula Kappa " );}
