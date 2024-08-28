4af2d604668da0077612210e149a6074e8b6c53d
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-27 23:56:24 -0300

    Create allparticipantes.js

---
+++ allparticipantes.js
@@ -0,0 +1,7 @@
+if (pregunta.includes("angelica") || pregunta.includes("angélica")) {(" 	💩😭 " );}
+  else if (pregunta.includes("carlyn")) {("te conozco hace 3 vidas" );}
+ else if (pregunta.includes("linda") || pregunta.includes("gitana")) {("	la perlan't" );}
+   else if (pregunta.includes("antonia")) {("	tengo sueño zzzzz " );}
+     else if (pregunta.includes("cami andrade") || pregunta.includes("andrade")) {("	Cosita linda cosita bien hecha " );}
+ else if (pregunta.includes("mich")) {("		producción pedorra " );}
+ else if (pregunta.includes("power") ||pregunta.includes("ppt")) {("		que injusto nooo que injustoo " );}
