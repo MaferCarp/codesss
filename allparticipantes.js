2d0408c0af276453f76cd9212bb0a786dea6f6fd
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-08-27 23:57:28 -0300

    Update allparticipantes.js

--- allparticipantes.js
+++ allparticipantes.js
@@ -5,3 +5,11 @@ if (pregunta.includes("angelica") || pregunta.includes("angélica")) {(" 	💩�
      else if (pregunta.includes("cami andrade") || pregunta.includes("andrade")) {("	Cosita linda cosita bien hecha " );}
  else if (pregunta.includes("mich")) {("		producción pedorra " );}
  else if (pregunta.includes("power") ||pregunta.includes("ppt")) {("		que injusto nooo que injustoo " );}
+else if (pregunta.includes("manu") || pregunta.includes("italiano")) {(" achepto " );}
+  else if (pregunta.includes("pedro")) {(" Pedro Pedro Pedro PEDRO PE! SingsNote " );}
+ else if (pregunta.includes("waldo")) {(" carrrcul Kappa " );}  
+   else if (pregunta.includes("felipe")) {(" 	Sapo parao :V PopNemo " );}
+     else if (pregunta.includes("ivan") || pregunta.includes("iván")) {(" ivan lo hizo antes Kappa " );}
+     	else if (pregunta.includes("iñigo")) {(" encerré a mi nana PopNemo " );}
+        	else if (pregunta.includes("yuhui")) {(" Te tengo miedo? Imposible! " );}
+else {}
