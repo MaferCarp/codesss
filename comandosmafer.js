d92f089f8badc98ebf4c050599c4b549704a1be4
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-09-05 00:13:48 -0300

    Create comandosmafer.js

---
+++ comandosmafer.js
@@ -0,0 +1,3 @@
+if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres") || pregunta.includes("liderazgo")) {("Patricio Líder de la semana")}   // quien/es, cual/es
+    
+ else {}
