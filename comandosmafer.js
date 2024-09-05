2166e63ab30230cfc76d7854c2a07bf291041890
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-09-05 00:17:29 -0300

    Update comandosmafer.js

--- comandosmafer.js
+++ comandosmafer.js
@@ -1,3 +1,3 @@
 if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres") || pregunta.includes("liderazgo")) {("Patricio Líder de la semana")}   // quien/es, cual/es
-    
+ else if (pregunta.includes("espontan") || pregunta.includes("espontán") || pregunta.includes("espont")) {("Manuel usó espontánea con 3 a Felipe y 2 a Waldo. "); }   
  else {}
