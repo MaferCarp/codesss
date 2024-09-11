3b41e8f8ea4e2a417f9652f19b8d7d0b8ccb0c06
MaferCarp <163231196+MaferCarp@users.noreply.github.com>
2024-09-11 16:40:57 -0300

    Update comandosmafer.js

--- comandosmafer.js
+++ comandosmafer.js
@@ -1,3 +1,3 @@
-if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres") || pregunta.includes("liderazgo")) {("Pedro Waldo Miguel y Linda x ser lider")}   // quien/es, cual/es
+if (pregunta.includes("líder") || pregunta.includes("líderes") || pregunta.includes("lider") || pregunta.includes("lideres") || pregunta.includes("liderazgo")) {("ᴘᴇᴅʀᴏ ᴘᴇᴅʀᴏ ᴘᴇᴅʀᴏ ᴘᴇᴅʀᴏ ᴘᴇ! SingsNote")}   // quien/es, cual/es
  else if (pregunta.includes("espontan") || pregunta.includes("espontán") || pregunta.includes("espont")) {("Manuel usó espontánea con 3 a Felipe y 2 a Waldo. "); }   
  else {}
