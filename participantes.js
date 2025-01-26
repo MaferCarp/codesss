d221b560966233d29e465d3c9a31e573b10b6c34
marcos cristian <163231196+MaferCarp@users.noreply.github.com>
2025-01-26 17:52:42 -0300

    Update participantes.js

--- participantes.js
+++ participantes.js
@@ -1,3 +1,4 @@
 if (pregunta.includes("tridente") || pregunta.includes("Tridente")) {(" 🍼🍼🍼 " );}
 else if (pregunta.includes("rocket") ||pregunta.includes("Rocket")) {("	🍼🍼🍼 " );}
+else if (pregunta.includes("power") ||pregunta.includes("Ranges")) {("	🍼🍼🍼 " );}
 else {}
