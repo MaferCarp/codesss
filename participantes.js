a8006c38d82ade1c3e3fd57d29a88a7bb23399fa
marcos cristian <163231196+MaferCarp@users.noreply.github.com>
2025-01-26 17:24:57 -0300

    Update participantes.js

--- participantes.js
+++ participantes.js
@@ -1,8 +1,3 @@
-if (pregunta.includes("angelica") || pregunta.includes("angélica")) {(" 	💩😭 " );}
-  else if (pregunta.includes("carlyn")) {("te conozco hace 3 vidas" );}
- else if (pregunta.includes("linda") || pregunta.includes("gitana")) {("	la perlan't" );}
-   else if (pregunta.includes("antonia")) {("	tengo sueño zzzzz " );}
-     else if (pregunta.includes("cami andrade") || pregunta.includes("andrade")) {("	Cosita linda cosita bien hecha " );}
- else if (pregunta.includes("mich")) {("		producción pedorra " );}
- else if (pregunta.includes("power") ||pregunta.includes("ppt")) {("		que injusto nooo que injustoo " );}
+if (pregunta.includes("tridente") || pregunta.includes("Tridente")) {(" 🍼🍼🍼 " );}
+else if (pregunta.includes("rocket") ||pregunta.includes("Rocket")) {("	🍼🍼🍼 " );}
 else {}
